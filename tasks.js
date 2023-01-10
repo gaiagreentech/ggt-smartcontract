100aa51b0ae808281fcf5137126b059b7d362b39
Davi Figueiredo <davi.mello.28@gmail.com>
2023-01-10 00:41:05 -0300

    fix: multiple tasks flows now allow custom address interaction

--- tasks.js
+++ tasks.js
@@ -1,29 +1,33 @@
 
 require("@nomiclabs/hardhat-web3");
 
-task("deploy", "Deploy the contract", async ( _, hre ) => {
-  // get current logged in account from metamask
-  const accounts = await hre.web3.eth.getAccounts()
+task("deploy", "Deploy the contract")
+   .addParam("interval", "Set the interval for all the tokens to be disposed", 30 * 24 * 60 * 60, types.int)
+   .setAction( async ( { interval }, hre ) => {
+      const accounts = await hre.web3.eth.getAccounts()
 
-	const WEEECycleNFT = await hre.ethers.getContractFactory("WEEECycleNFT", accounts[0]);
+      const WEEECycleNFT = await hre.ethers.getContractFactory("WEEECycleNFT", accounts[0]);
 
-	const deploy = await WEEECycleNFT.deploy(1000);
+      const deploy = await WEEECycleNFT.deploy(interval);
 
-	await deploy.deployed();
+      await deploy.deployed();
 
-	console.log("WEEECycleNFT deployed to:", deploy.address);
-})
+      console.log("WEEECycleNFT deployed to:", deploy.address);
+  })
 
 task("mint", "Give the address argument one NFT")
+  .addParam("contract", "Set the address of the contract")
   .addParam("address", "Set the address to mint the token")
+  .addParam("uri", "Set the NFT data. Preferably a IPFS url")
   .setAction(async ( args, hre ) => {
     const accounts = await hre.web3.eth.getAccounts()
+    const signer = await hre.ethers.getSigner(accounts[0])
 
-    const WEEECycleNFT = await hre.ethers.getContractAt("WEEECycleNFT", accounts[0])
+    const WEEECycleNFT = await hre.ethers.getContractAt("WEEECycleNFT", args.contract)
 
-    const tx = await WEEECycleNFT.connect(accounts[0]).safeMint(args.address)
+    const tx = await WEEECycleNFT.connect(signer).safeMint(args.address, args.uri)
 
-    await tx.wait()
+    const txEnd = await tx.wait()
 
-    console.log(`Minted NFT to ${args.address} on transaction ${tx.id}` )
+    console.log(`Minted NFT to ${args.address} on transaction ${txEnd.transactionHash}` )
 })
